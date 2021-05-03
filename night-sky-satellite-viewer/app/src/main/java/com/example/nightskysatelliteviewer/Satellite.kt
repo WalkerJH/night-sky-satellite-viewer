80d98d9c9f6b23e7ebfbc16cdb2e8536d426c019
Walker Herring <72236362+WalkerJH@users.noreply.github.com>
2021-05-03 02:24:25 -0700

    Preliminary work creating Satellite object and downloader class

---
+++ night-sky-satellite-viewer/app/src/main/java/com/example/nightskysatelliteviewer/Satellite.kt
@@ -0,0 +1,14 @@
+package com.example.nightskysatelliteviewer
+
+import java.util.*
+
+class Satellite(val name: String, val id: String, val epoch: Date,
+                val mean_motion: Double, val eccentricity: Double,
+                val inclination: Double, val raOfAscNode: Double,
+                val argOfPericenter: Double, val meanAnomaly: Double,
+                val empherisType: Int, val classificationType: String,
+                val noradCatId: String, val elementSetNo:Int,
+                val revAtEpoch: Int, val bStar: Double,
+                val meanMotionDot: Double, val meanMotionDDot: Double) {
+
+}
