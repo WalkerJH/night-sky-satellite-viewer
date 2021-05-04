26e0c94db05ff7ac269a98a1b876fe903b2d2bee
Walker Herring <72236362+WalkerJH@users.noreply.github.com>
2021-05-03 20:05:28 -0700

    initial parsing of xml

--- night-sky-satellite-viewer/app/src/main/java/com/example/nightskysatelliteviewer/SatelliteDataList.kt
+++ night-sky-satellite-viewer/app/src/main/java/com/example/nightskysatelliteviewer/SatelliteDataList.kt
@@ -1,6 +1,10 @@
 package com.example.nightskysatelliteviewer
 
+import org.w3c.dom.Document
 import java.net.URL
+import org.xml.sax.InputSource
+import java.io.StringReader
+import javax.xml.parsers.*
 
 const val satelliteXmlUrlText = "http://www.celestrak.com/NORAD/elements/gp.php?GROUP=active&FORMAT=xml"
 
@@ -12,10 +16,20 @@ object SatelliteDataList {
 
     fun updateAll() {
         val xmlText = satelliteXmlUrl.readText()
+
+        val xmlDoc = stringToXmlDoc(xmlText)
+        val root = xmlDoc.firstChild
         //TODO: update all satellite objects
     }
 
     fun updateSatellite(id: Int) {
         //TODO: update satellite with given id
+    }
+
+    private fun stringToXmlDoc(text: String): Document {
+        val factory: DocumentBuilderFactory = DocumentBuilderFactory.newInstance()
+        val builder: DocumentBuilder = factory.newDocumentBuilder()
+        val xmlDoc = builder.parse(InputSource(StringReader(text)))
+        return xmlDoc
     }
 }
