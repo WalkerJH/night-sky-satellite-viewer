02dac7c55b8f406d9a03bd6d4bf2410c851933fe
Walker Herring <72236362+WalkerJH@users.noreply.github.com>
2021-05-03 21:17:57 -0700

    updating all satellites from xml

--- night-sky-satellite-viewer/app/src/main/java/com/example/nightskysatelliteviewer/Satellite.kt
+++ night-sky-satellite-viewer/app/src/main/java/com/example/nightskysatelliteviewer/Satellite.kt
@@ -2,14 +2,10 @@ package com.example.nightskysatelliteviewer
 
 import java.util.*
 
-class Satellite(val name: String, val id: String, val epoch: Date,
+class Satellite(val name: String, val id: String, val epoch: Long,
                 val mean_motion: Double, val eccentricity: Double,
                 val inclination: Double, val raOfAscNode: Double,
-                val argOfPericenter: Double, val meanAnomaly: Double,
-                val empherisType: Int, val classificationType: String,
-                val noradCatId: String, val elementSetNo:Int,
-                val revAtEpoch: Int, val bStar: Double,
-                val meanMotionDot: Double, val meanMotionDDot: Double) {
+                val argOfPericenter: Double, val meanAnomaly: Double) {
 
 
 
--- night-sky-satellite-viewer/app/src/main/java/com/example/nightskysatelliteviewer/SatelliteDataList.kt
+++ night-sky-satellite-viewer/app/src/main/java/com/example/nightskysatelliteviewer/SatelliteDataList.kt
@@ -1,12 +1,18 @@
 package com.example.nightskysatelliteviewer
 
 import org.w3c.dom.Document
-import java.net.URL
+import org.w3c.dom.Element
+import org.w3c.dom.NodeList
 import org.xml.sax.InputSource
 import java.io.StringReader
-import javax.xml.parsers.*
+import java.net.URL
+import java.text.SimpleDateFormat
+import java.util.*
+import javax.xml.parsers.DocumentBuilder
+import javax.xml.parsers.DocumentBuilderFactory
 
 const val satelliteXmlUrlText = "http://www.celestrak.com/NORAD/elements/gp.php?GROUP=active&FORMAT=xml"
+const val epochDateFormat = "yyyy-MM-dd'T'HH:mm:ss.SSSSSS"
 
 object SatelliteDataList {
 
@@ -14,22 +20,47 @@ object SatelliteDataList {
 
     private val satelliteXmlUrl = URL(satelliteXmlUrlText)
 
+    fun updateSatellite(id: Int) {
+        //TODO: update satellite with given id
+    }
+
     fun updateAll() {
         val xmlText = satelliteXmlUrl.readText()
 
         val xmlDoc = stringToXmlDoc(xmlText)
-        val root = xmlDoc.firstChild
-        //TODO: update all satellite objects
+        val satelliteXmlElements: NodeList? = xmlDoc.getElementsByTagName("body")
+        if (satelliteXmlElements != null) {
+            for (i in 0 until satelliteXmlElements.length) {
+                satellites.add(createSatelliteFromXml(satelliteXmlElements.item(i) as Element))
+            }
+        }
     }
 
-    fun updateSatellite(id: Int) {
-        //TODO: update satellite with given id
+    private fun createSatelliteFromXml(xmlElement: Element): Satellite {
+        val name = getElementContent(xmlElement, "OBJECT_NAME")
+        val id = getElementContent(xmlElement, "OBJECT_ID")
+        val epoch = parseDate(getElementContent(xmlElement, "EPOCH"))
+        val mean_motion = getElementContent(xmlElement, "MEAN_MOTION") as Double
+        val eccentricity = getElementContent(xmlElement, "ECCENTRICITY") as Double
+        val inclination = getElementContent(xmlElement, "INCLINATION") as Double
+        val raOfAscNode = getElementContent(xmlElement, "RA_OF_ASC_NODE") as Double
+        val argOfPericenter = getElementContent(xmlElement, "ARG_OF_PERICENTER") as Double
+        val meanAnomaly = getElementContent(xmlElement, "MEAN_ANOMALY") as Double
+        return Satellite(name, id, epoch, mean_motion, eccentricity, inclination,
+                            raOfAscNode, argOfPericenter, meanAnomaly)
+    }
+
+    private fun parseDate(dateString: String): Long {
+        return SimpleDateFormat(epochDateFormat).parse(dateString).getTime()
+    }
+
+    private fun getElementContent(element: Element, name: String): String {
+        return element.getElementsByTagName(name).item(0).textContent
     }
 
     private fun stringToXmlDoc(text: String): Document {
         val factory: DocumentBuilderFactory = DocumentBuilderFactory.newInstance()
         val builder: DocumentBuilder = factory.newDocumentBuilder()
-        val xmlDoc = builder.parse(InputSource(StringReader(text)))
-        return xmlDoc
+        return builder.parse(InputSource(StringReader(text)))
     }
 }
